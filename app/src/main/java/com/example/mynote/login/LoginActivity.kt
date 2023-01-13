9e77d5995bba741c599c66acbd22998bf83b5e12
nabilaabdiel <nabila.abdiel2@gmail.com>
2023-01-13 10:02:26 +0700

    Layouting on process

---
+++ app/src/main/java/com/example/mynote/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mynote.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynote.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/example/mynote/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mynote.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynote.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
