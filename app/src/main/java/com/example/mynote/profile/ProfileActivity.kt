d598913f49cfc5ed38a2ccfea8da759f7126a647
nabilaabdiel <nabila.abdiel2@gmail.com>
2023-01-17 15:02:09 +0700

    biar ga hilang

---
+++ app/src/main/java/com/example/mynote/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mynote.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynote.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
